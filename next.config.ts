7b0380711cf5a770a762319efd6c90038915e153
Joseph Ilhan <joseph@josephilhan.com>
2025-08-09 03:12:44 +0200

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  
+  // Output standalone for Docker optimization
+  output: 'standalone',
 };
 
 export default nextConfig;
